56cb24d1ba55036da52cc9b5869027cc247a561d
Steve G <ssghostfacekillah@hotmail.com>
2022-10-14 14:07:28 +0100

    audio support

--- SethSpelling.py
+++ SethSpelling.py
@@ -1,6 +1,17 @@
 # version 1.0.0
 # -*- coding: utf-8 -*-
 
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+import pygame  # it is important to import pygame after that
+
+# need pygame installed - pip install pygame
+import time
+from pygame import mixer
+
+
+
 pound = u'\u00A3'
 
 print("Seth's spelling test v1.0.0")
@@ -18,7 +29,7 @@ rightAnswers = 0
 wrongAnswers = 0
 
 # put the selling items below!
-answerOne = str("one")
+answerOne = str("hello")
 answerTwo = str("two")
 answerThree = str("three")
 answerFour = str("four")
@@ -29,13 +40,46 @@ answerEight = str("eight")
 answerNine = str("nine")
 answerTen = str("ten")
 
-wordOne = input("Spell the first word...\n")
+wordNumber = str("\nFirst word")
 
+def play_1():
+    print(f"{wordNumber}")
+    mixer.init()
+    mixer.music.load("C:\Audio\hello.mp3")
+    mixer.music.play()
+    while mixer.music.get_busy():  # wait for music to finish playing
+        time.sleep(1)
+
+
+def repeatWord():
+    repNum = 1
+    while repNum >= 1:
+        hearAgain = input("\nWould you like to hear it again?\n")
+        if hearAgain == str("yes"):
+            repNum = 1
+            play_1()
+        else:
+            repNum = 0
+            print("\n")
+
+
+
+startSpell = input("\nPress any key for the first word...\n")
+if startSpell == str("exit"):
+    exit()
+else:
+    wordNumber = str("\nFirst word")
+    play_1()
+    repeatWord()
+
+
+    
+wordOne = input("Spell the first word...\n")
 if wordOne == answerOne:
     print("\nCorrect, well done!")
     rightAnswers += 1
 else:
-    print(f'\nIncorrect! You put "{wordOne}", the answer is "{answerOne}"')
+    print(f'\nIncorrect! You put "{wordOne}", the answer is "{answerOne}".')
     wrongAnswers += 1
 
 # End of test
